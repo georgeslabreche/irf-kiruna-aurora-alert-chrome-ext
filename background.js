cd530f705d605fc6887124503581b8dd12dedeaa
Georges Labrèche <georges@opendatakosovo.org>
2018-03-16 14:04:29 +0100

    improved notification system.

--- background.js
+++ background.js
@@ -1,9 +1,28 @@
-var pollInterval = 60 * 1000 ; // 1 minute in milliseconds
+const K_INDEX_URL = 'http://www2.irf.se/maggraphs/preliminary_real_time_k_index_15_minutes';
+const MAG_URL = 'http://www2.irf.se/maggraphs/rt_iaga_last_hour.txt';
+
+const POLL_INTERVALL = 60 * 1000 ; // 1 minute in milliseconds
+
+const K_INDEX_COLOR_GRADIENTS = [
+    [23, 9, 149, 255],  // 0
+    [32, 38, 132, 255], // 1
+    [42, 68, 116, 255], // 2
+    [51, 97, 100, 255], // 3
+    [61, 127, 84, 255], // 4
+    [70, 156, 68, 255], // 5
+    [80, 186, 52, 255], // 6
+    [89, 215, 36, 255], // 7
+    [99, 245, 20, 255]  // 8
+    [99, 245, 20, 255]  // 9 // Update
+]
 
 function pollIRF() {
-    const url = 'http://www2.irf.se/maggraphs/rt_iaga_last_hour.txt';
+    fetchAndProcessMagReadings();
+    window.setTimeout(pollIRF, POLL_INTERVALL);
+}
 
-    fetch(url)
+function fetchAndProcessMagReadings(){
+    fetch(MAG_URL)
         .then(function(response) {
             response.text().then(function (text) {
                 // Init arrays that will store magnetogram readings for KIRX, KIRY, KIRZ, and KIRF.
@@ -58,26 +77,42 @@ function pollIRF() {
                     }
                 }
 
+
+                // We can only displa 1 number in the badge.
+                // We choose the Z index value indicating measurements above.
+                analyzeTrend(kirkZArray);
+
                 // A high KIRKZ value is usually best so we check for this first.
                 // We only care if in the past hour and a half we reached at least the 52000 nT values for KIRKZ.
                 if(ss.max(kirkZArray) >= 52000){
-                    analyzeTrend(kirkZArray);
+                    var highZComponentNotification = {
+                        type: "basic",
+                        title: "Look up above you!",
+                        message: "Over 52,000 nT was measured above you about an hour ago.",
+                        iconUrl: "icons/icon-128.png"
+                    };
+
+                    chrome.notifications.create("success-notification", highZComponentNotification);
                 }
+
+                //TODO: How about KIRKX? Need to understand the thresholds more...
 
                 // A high KIRKY value is usually second best so we check for this only after checking for high KIRKZ. 
                 // We only care if in the past hour and a half we reached at least the 100 nT values for KIRKY.
                 else if(ss.max(kirkYArray) >= 100){
-                    analyzeTrend(kirkYArray);
+                    var highYComponentNotification = {
+                        type: "basic",
+                        title: "Look to the East-West!",
+                        message: "Over 100 nT was measured about an hour ago.",
+                        iconUrl: "icons/icon-128.png"
+                    };
+
+                    chrome.notifications.create("success-notification", highYComponentNotification);
                 }
 
-                else{
-                    noNotification();
-                }
   
             });
-        })
-
-    window.setTimeout(pollIRF, pollInterval);
+        });
 }
 
 // Analyze trend of magnetogram values and notify if event was likely missed.
@@ -95,21 +130,20 @@ function analyzeTrend(kirkArray){
     var lr = ss.linearRegression(timeKirkPairs);
     var lrSlope = lr['m'];
 
-    // If linear regression trend for KIRKZ value is that it is increasing,
-    // set badge to green indicating chances of aurora sighting.
-    if(lrSlope >= 1){
-        chrome.browserAction.setBadgeBackgroundColor({color: [50, 205, 50, 255]});
-    }else{
-        // If it is decrease, badge is grey.
-        chrome.browserAction.setBadgeBackgroundColor({color: [211, 211, 211, 255]});
-    }
-
-    // Display linear regression slope value as badge text.
-    chrome.browserAction.setBadgeText({text: lrSlope.toString().substring(0, 4).replace('.0','')});
+    updateBadge(lrSlope);      
 }
 
-function noNotification(){
-    chrome.browserAction.setBadgeText({text: ''});
+function updateBadge(lrSlope){
+    fetch(K_INDEX_URL)
+        .then(function(response) {
+            response.text().then(function (text) {
+                // Determine badge color based on Kp Index
+                chrome.browserAction.setBadgeBackgroundColor({color: K_INDEX_COLOR_GRADIENTS[parseInt(text)]});
+
+                // Display linear regression slope value as badge text.
+                chrome.browserAction.setBadgeText({text: lrSlope.toString().substring(0, 4)});
+            });
+        });  
 }
 
 
